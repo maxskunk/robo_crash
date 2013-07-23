052057ad4377a1e184218229ad8aaac1c0fa1d79
manno <manno78@yahoo.com>
2013-07-23 01:05:16 -0400

    changed fire script

--- unity_project/Assets/Scripts/fire.js
+++ unity_project/Assets/Scripts/fire.js
@@ -1,6 +1,47 @@
 var weaponStrengthMultiplier:int = 1;
 var projectileMass:int = 1;
 var projectileSpeed:int = 10000;
+var projectileScale:int = 1;
+var projectileLifeSpan:int = 20;
+var projectilePrefab:GameObject;
+
+
+
+
+
+function Start () 
+{
+
+}
+//TODO: This script will have to be fine tuned along with mass and friction of blocks to give the most entertinaing "Feel" to the game
+function Update () 
+{
+	if(Input.GetButtonDown("Fire1"))
+	{
+		//TODO: This will ultimatly be a cross hair
+		var ray:Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//var projectileObject:GameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		projectileObject = Instantiate(projectilePrefab, transform.position, transform.rotation);
+		projectileObject.transform.localScale.x = projectileScale;
+		projectileObject.transform.localScale.y = projectileScale;
+		projectileObject.transform.localScale.z = projectileScale;
+		projectileObject.transform.position = ray.origin;
+		projectileObject.AddComponent("Rigidbody");
+		projectileObject.rigidbody.mass = projectileMass;
+		projectileObject.rigidbody.AddForce(ray.direction * projectileSpeed * weaponStrengthMultiplier );
+		Destroy(projectileObject, projectileLifeSpan);//Destroy the game object in 100 seconds.
+	}
+	
+	//TODO: We will need to clean up old projectiles in 1 or 2 ways
+	// a. Timer
+	// b. When they hit the plane
+}
+
+
+/*
+var weaponStrengthMultiplier:int = 1;
+var projectileMass:int = 1;
+var projectileSpeed:int = 10000;
 var projectileScale:int = 10;
 
 
@@ -30,3 +71,4 @@ function Update ()
 	// a. Timer
 	// b. When they hit the plane
 }
+*/
